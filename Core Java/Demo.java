7bc7ce40543fc937dc11820f3e66a8530183a621
Selvam <128716393+Selvam-18@users.noreply.github.com>
2026-02-27 20:37:54 +0530

    core-java

---
+++ Core Java/Demo.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import tools.Calc;
+
+// class Student{
+//     int age;
+//     String name;
+//     double marks;
+// }
+
+// class Methods{
+//     public static void printWelcome() {
+//         System.out.println("Welcome");
+//     }
+// }
+
+// class Human{
+//     private int age = 23;
+//     private String name = "Selvam";
+
+//     public String getValues() {
+//         return this.name + " : " + this.age;
+//     }
+
+//     public void setValues(int a, String n) {
+//         this.age = a;
+//         this.name = n;
+//     }
+// }
+
+// class A{
+//     public void welcome(){
+//         System.out.println("Welcome");
+//     }
+// }
+
+// class B extends A{
+//     public void welcome(){
+//         System.out.println("Wubba Dubba Lubba Dub");
+//     }
+// }
+
+public class Demo {
+    public static void main(String args[]) {
+
+        Calc obj = new Calc();
+        // A obj = new A();
+        // B obj1 = new B();
+        // obj.welcome();
+        // obj1.welcome();
+
+        // AdvCalc obj = new AdvCalc();
+        // System.out.println("Add: " + obj.add(1, 2));
+        // System.out.println("Sub: " + obj.sub(2, 1));
+        // System.out.println("Mul: " + obj.mul(2, 1));
+        // System.out.println("Div: " + obj.div(2, 1));
+        // String x = "Hello World!";
+        // int n = 10;
+        
+        // Student obj = new Student();
+        // obj.age = 23;
+        // obj.name = "Selvam";
+        // obj.marks = 80;
+
+        // Student stud[] = new Student[2];
+        // stud[0] = obj;
+
+        // int nums[] = new int[5];
+        // for(int i=0; i<nums.length; i++) {
+        //     nums[i] = i;
+        // }
+
+        // for(int value: nums) {
+        //     System.out.println(value + " ");
+        // }
+
+        // System.out.println(Arrays.toString(nums) + " Array");
+
+        // Methods.printWelcome();
+
+        // Human obj1 = new Human();
+        // Human obj2 = new Human();
+        // System.out.println(obj1.getValues());
+        // obj2.setValues(22, "mavleS");
+        // System.out.println(obj2.getValues());
+
+
+        // System.out.println(stud[0].name);
+
+
+        // switch(n) {
+        //     case 1: 
+        //         System.out.println("One");
+        //     case 10: 
+        //         System.out.println("Ten");
+        //         break;
+        //     default:
+        //         System.out.println("Nothing");
+        // }
+        // System.out.println(x);
+    }
+}
