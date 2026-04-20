1e4d9cfd7ee03376483c8ac4dcf2bfb4b86f61eb
Selvam <128716393+Selvam-18@users.noreply.github.com>
2026-04-20 23:22:29 +0530

    Job portal app with REST API

---
+++ JobApp with REST/src/main/java/com/example/JobApp/JobRestController.java
@@ -0,0 +1,47 @@
+package com.example.JobApp;
+
+
+import com.example.JobApp.model.JobPost;
+import com.example.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping("posts")
+    public List<JobPost> getAllJobs() {
+        return service.getAllJobs();
+    }
+
+    @GetMapping("posts/{postId}")
+    public JobPost getJob(@PathVariable("postId") int postId) {
+        return service.getJob(postId);
+    }
+
+    @PostMapping("jobpost")
+    public JobPost addJob(@RequestBody JobPost newJobPost) {
+        service.addJob(newJobPost);
+        return service.getJob(newJobPost.getPostId());
+    }
+
+    @PutMapping("jobpost")
+    public JobPost updateJob(@RequestBody JobPost updatedJob) {
+        service.updateJob(updatedJob);
+        return service.getJob(updatedJob.getPostId());
+    }
+
+    @DeleteMapping("jobpost/{postId}")
+    public String deleteJob(@PathVariable("postId") int postId) {
+        service.deleteJob(postId);
+
+        return "Post Deleted";
+    }
+}
